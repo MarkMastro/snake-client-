0a6e7c9cc3f5e67ab68f21b8a4957aa471889caa
Mark Mastropaolo <mastropaolo.mark@gmail.com>
2021-08-23 23:51:32 +0000

    add ctrl+c functionality to exit game properly

--- play.js
+++ play.js
@@ -1,3 +1,22 @@
 const {connect}=require('./client')
 
 connect()
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data',(key)=>handleUserInput(key))
+  
+  return stdin;
+}
+const handleUserInput=function(key){
+  if (key === '\u0003') {
+      console.log('ctrl+c pressed, exit')
+      process.exit();
+    
+  }
+}
+
+setupInput();
