b32a83ea5d146d059b0c4fce67966569d8b145fe
Mark Mastropaolo <mastropaolo.mark@gmail.com>
2021-08-24 00:04:24 +0000

    add stdin functions from play.js to here

---
+++ input.js
@@ -0,0 +1,17 @@
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data',(key)=>handleUserInput(key))
+  
+  return stdin;
+}
+const handleUserInput=function(key){
+  if (key === '\u0003') {
+      console.log('ctrl+c pressed, exit')
+      process.exit();
+    
+  }
+}
+module.exports={setupInput}
