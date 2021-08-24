8ff401d877bc05270193b352b5477024f3855a3b
Mark Mastropaolo <mastropaolo.mark@gmail.com>
2021-08-24 01:34:50 +0000

    add connection object to std handler to write to server, as well as movement commands

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection=conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -8,10 +11,24 @@ const setupInput = function() {
   return stdin;
 }
 const handleUserInput=function(key){
-  if (key === '\u0003') {
+  switch (key){
+    case '\u0003':
       console.log('ctrl+c pressed, exit')
       process.exit();
-    
+      break;
+    case 'w':
+      connection.write('Move: up')
+      break;
+    case 'a':
+      connection.write('Move: left')
+      break;
+    case 'd':
+      connection.write('Move: right')
+      break;
+    case 's':
+      connection.write('Move: down')
+      break;
   }
+ 
 }
 module.exports={setupInput}
